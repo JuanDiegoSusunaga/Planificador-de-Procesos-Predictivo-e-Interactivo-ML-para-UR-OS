c02ea6f4a10d5952f245eea0ed3955ae9415df3b
geronimorojasg3 <geronimorojasgurosario@gmail.com>
2026-05-20 22:06:13 -0500

    Create TelemetryManager.java

---
+++ src/ur_os/telemetry/TelemetryManager.java
@@ -0,0 +1,53 @@
+package ur_os.telemetry;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TelemetryManager {
+    private static TelemetryManager instance;
+    private BufferedWriter writer;
+
+    private final String CSV_PATH = "ml_training/datasets/telemetry_data.csv";
+
+    private TelemetryManager() {
+        try {
+            File directory = new File("ml_training/datasets");
+            if (!directory.exists()) {
+                directory.mkdirs(); 
+            }
+            File file = new File(CSV_PATH);
+            boolean isNewFile = !file.exists();
+            
+            writer = new BufferedWriter(new FileWriter(file, true));
+            if (isNewFile) {
+                // Cabeceras exactas del Dataset
+                writer.write("ProcessID,UserIntent,CpuBursts,IoBlocks,TurnaroundTime");
+                writer.newLine();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            System.err.println("Error iniciando telemetría: " + e.getMessage());
+        }
+    }
+
+    public static TelemetryManager getInstance() {
+        if (instance == null) {
+            instance = new TelemetryManager();
+        }
+        return instance;
+    }
+
+    public void exportProcessData(int id, String intent, int cpuBursts, int ioBlocks, int turnaroundTime) {
+        if (writer != null) {
+            try {
+                writer.write(id + "," + intent + "," + cpuBursts + "," + ioBlocks + "," + turnaroundTime);
+                writer.newLine();
+                writer.flush();
+            } catch (IOException e) {
+                System.err.println("Error guardando datos del proceso: " + id);
+            }
+        }
+    }
+}
