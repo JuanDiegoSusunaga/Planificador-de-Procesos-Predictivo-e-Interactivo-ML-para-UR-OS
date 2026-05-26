c2c2b9d02c2d38c4c357e138f800ed07aef86bbd
geronimorojasg3 <geronimorojasgurosario@gmail.com>
2026-05-26 17:10:19 -0500

    Update CPU.java

--- src/ur_os/system/CPU.java
+++ src/ur_os/system/CPU.java
@@ -1,7 +1,6 @@
 package ur_os.system;
 
 import ur_os.memory.Memory;
-import ur_os.memory.MemoryInstruction;
 import ur_os.memory.freememorymagament.MemorySlot;
 import ur_os.process.CPUInstruction;
 import ur_os.process.Instruction;
@@ -10,139 +9,147 @@ import ur_os.process.ProcessInstructionType;
 import ur_os.process.ProcessState;
 import ur_os.virtualmemory.SwapMemory;
 
-/**
- *
- * @author super
- */
 public class CPU {
-    
+
     Process p;
     OS os;
     MemoryUnit mu;
-    
-    public CPU(){
-        this(null,null,null);
+
+    public CPU() {
+        this(null, null, null);
     }
-    
-    public CPU(OS os, Memory m, SwapMemory s){
+
+    public CPU(OS os, Memory m, SwapMemory s) {
         this.os = os;
-        if(os != null)
-            this.mu = new MemoryUnit(m,s,this,os.smm);
-        else
-            this.mu = new MemoryUnit(m,s,this,null);
+
+        if (os != null) {
+            this.mu = new MemoryUnit(m, s, this, os.smm);
+        } else {
+            this.mu = new MemoryUnit(m, s, this, null);
+        }
     }
-    
-    public CPU(Memory m, SwapMemory s){
-        this(null,m,s);
+
+    public CPU(Memory m, SwapMemory s) {
+        this(null, m, s);
     }
-    
-    public void setOS(OS os){
+
+    public void setOS(OS os) {
         this.os = os;
         mu.setSMM(os.smm);
     }
-    
-    public void addProcess(Process p){
+
+    public void addProcess(Process p) {
         this.p = p;
         p.setState(ProcessState.CPU);
-        // Se eliminó la línea p.addCpuBurst() para evitar contar ingresos estáticos.
     }
-    
-    public Process getProcess(){
+
+    public Process getProcess() {
         return p;
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return p == null;
     }
-    
-    public void update(){
-        if(!isEmpty()) {
-            // Modificado: Si la instrucción actual es de CPU, registramos el ciclo de cómputo real
-            if (p.getCurrentInstruction() != null && p.getCurrentInstruction().getType() == ProcessInstructionType.CPU) {
+
+    public void update() {
+        if (!isEmpty()) {
+            if (
+                    p.getCurrentInstruction() != null &&
+                    p.getCurrentInstruction().getType() == ProcessInstructionType.CPU
+            ) {
                 p.addCpuCycles(1);
             }
+
             advanceInstruction();
         }
+
         mu.update();
     }
-    
-    public MemoryUnit getMemoryUnit(){
+
+    public MemoryUnit getMemoryUnit() {
         return mu;
     }
-    
-    public void addProcessToMemoryUnit(Process p){
+
+    public void addProcessToMemoryUnit(Process p) {
         mu.addProcess(p);
     }
-    
-    public void advanceInstruction(){
+
+    public void advanceInstruction() {
         Instruction i = p.getCurrentInstruction();
         p.advanceInstruction();
+
         Process tempp;
-        switch(i.getType()){
-            case MEMORY -> {
+
+        switch (i.getType()) {
+            case MEMORY:
                 System.out.println("Executing Memory instruction");
                 tempp = removeProcess();
                 os.interrupt(InterruptType.CPU_TO_MEMORY, tempp);
-            }
-                
-            case IO -> {
+                break;
+
+            case IO:
                 System.out.println("Executing IO instruction");
                 p.addIoBlock();
                 tempp = removeProcess();
                 os.interrupt(InterruptType.CPU_TO_IO, tempp);
-            }
-                
-            case CPU -> executeCPUOperation((CPUInstruction) i);
-            
-            case END -> {
+                break;
+
+            case CPU:
+                executeCPUOperation((CPUInstruction) i);
+                break;
+
+            case END:
                 tempp = removeProcess();
                 os.interrupt(InterruptType.FINISH_PROCESS, tempp);
-            }
-            default -> throw new AssertionError(i.getType().name());
+                break;
+
+            default:
+                throw new AssertionError(i.getType().name());
         }
     }
-    
-    public void interrupt(InterruptType t, Process p, Instruction i){
+
+    public void interrupt(InterruptType t, Process p, Instruction i) {
         os.interrupt(t, p, i);
     }
-    
-    public void executeCPUOperation(CPUInstruction i){
+
+    public void executeCPUOperation(CPUInstruction i) {
         System.out.println("Executing CPU instruction");
     }
-    
-    public Process removeProcess(){
+
+    public Process removeProcess() {
         Process t = p;
         p = null;
         return t;
     }
-    
-    public Process extractProcess(){
+
+    public Process extractProcess() {
         Process temp = p;
         p = null;
         return temp;
     }
-    
+
     @Override
-    public String toString(){
-        if(!isEmpty())
-            return "CPU: "+p.toString();
-        else
+    public String toString() {
+        if (!isEmpty()) {
+            return "CPU: " + p.toString();
+        } else {
             return "CPU: Empty";
+        }
     }
 
     void loadSlot(MemorySlot m, MemorySlot vm) {
-        mu.loadSlot(m,vm);
+        mu.loadSlot(m, vm);
     }
-    
+
     void storeSlot(MemorySlot m, MemorySlot vm) {
-        mu.storeSlot(m,vm);
+        mu.storeSlot(m, vm);
     }
-    
+
     void loadPage(int frameMem, int frameSwap) {
-        mu.loadPage(frameMem,frameSwap);
+        mu.loadPage(frameMem, frameSwap);
     }
-    
+
     void storePage(int frameMem, int frameSwap) {
-        mu.storePage(frameMem,frameSwap);
+        mu.storePage(frameMem, frameSwap);
     }
 }
